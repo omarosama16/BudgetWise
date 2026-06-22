f19c3331851559a4bfa0ff70a6196c813496a745
MazenSaeed21 <mazensaeed214@gmail.com>
2026-06-22 21:16:36 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Home_Page/component/Header/Header.jsx'
+import Footer from './Home_Page/component/Footer/Footer.jsx'
+
+function App() {
+    
+    return(
+        <>
+        <Header></Header>
+        <Footer></Footer>
+        </>
+    );
+}
+
+export default App
